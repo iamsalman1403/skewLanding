a16bff7f8ceeacbe72614764f793277c99896ea1
Salman Parkar <39862393+iamsalman1403@users.noreply.github.com>
2018-10-18 21:24:26 +0530

    Add files via upload

---
+++ skew.js
@@ -0,0 +1,11 @@
+function openMenu(){
+    document.getElementById('side-menu').style.width = '250px';
+}
+
+function closeMenu() {
+    document.getElementById('side-menu').style.width = '0px';
+}
+
+function shutMenu() {
+    document.getElementById('side-menu').style.width = '0px';
+}
